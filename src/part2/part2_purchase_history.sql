d7840479eb178b33a48cce36de51c814f0d52a9c
mortylar <mortylar@student.21-school.ru>
2023-12-09 07:05:58 +0700

    Repair Purchase History view

--- src/part2/part2_purchase_history.sql
+++ src/part2/part2_purchase_history.sql
@@ -1,4 +1,7 @@
 DROP MATERIALIZED VIEW IF EXISTS PurchaseHistory CASCADE;
+
+
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS PurchaseHistory AS (
 
 SELECT cards.customer_id AS "Customer_ID", 
@@ -13,4 +16,5 @@ JOIN cards ON cards.customer_card_id = transaction.customer_card_id
 JOIN checks ON checks.transaction_id = transaction.transaction_id
 JOIN productgrid ON productgrid.sku_id = checks.sku_id
 JOIN stores ON stores.sku_id = checks.sku_id
+            AND transaction.transaction_store_id = stores.transaction_store_id
 GROUP BY cards.Customer_ID, transaction.Transaction_ID, Transaction_DateTime, Group_ID)
