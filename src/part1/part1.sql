1e67d45a767a9e1f502215bc3cbe47e2daf68d83
mortylar <mortylar@student.21-school.ru>
2023-12-17 11:36:56 +0700

    Complete part1

--- src/part1/part1.sql
+++ src/part1/part1.sql
@@ -7,6 +7,13 @@ DROP TABLE IF EXISTS Stores CASCADE;
 DROP TABLE IF EXISTS SKUGRoup CASCADE;
 DROP TABLE IF EXISTS DateOfAnalysisFormation CASCADE;
 
+DROP PROCEDURE IF EXISTS proc_export(table_name text, file_name text, format text, separator char(1));
+DROP PROCEDURE IF EXISTS proc_import(table_name text, file_name text, format text, separator char(1));
+
+DROP PROCEDURE IF EXISTS proc_export_all(p_directory text, p_format text, p_separator char(1));
+DROP PROCEDURE IF EXISTS proc_import_all(p_directory text, p_format text, p_separator char(1));
+
+
 SET DATESTYLE TO German;
 
 CREATE TABLE IF NOT EXISTS PersonalInformation(
@@ -59,8 +66,6 @@ CREATE TABLE IF NOT EXISTS Transaction(
                              Transaction_Store_Id INTEGER,
                              CONSTRAINT fk_Transaction_Customer_Card_Id FOREIGN KEY (Customer_Card_Id)
                                            REFERENCES Cards(Customer_Card_Id)
---                           CONSTRAINT fk_Transaction_Customer_Store_Id FOREIGN KEY (Transaction_Store_Id)
---                                         REFERENCES Stores(Transaction_Store_Id)
 );
 
 
@@ -81,5 +86,64 @@ CREATE TABLE IF NOT EXISTS Checks(
 CREATE TABLE IF NOT EXISTS DateOfAnalysisFormation(
                              Analysis_Formation TIMESTAMP WITHOUT TIME ZONE);
 
+
+
+CREATE OR REPLACE PROCEDURE proc_export(table_name text, file_name text, format text,
+                                        separator char(1) DEFAULT E'\t')
+LANGUAGE plpgsql
+AS $$
+DECLARE end_separator char(1) := (SELECT CASE WHEN (format = 'TSV')
+                                                THEN E'\t'
+                                              ELSE separator END);
+BEGIN
+  EXECUTE 'COPY ' || table_name || ' TO ' || '''' 
+                  || file_name || '''' 
+                  || ' WITH (FORMAT CSV, HEADER, DELIMITER ' 
+                  || '''' || end_separator || '''' || ')';
+END
+$$;
+
+
+CREATE OR REPLACE PROCEDURE proc_export_all(p_directory text, p_format text, p_separator char(1) DEFAULT E'\t')
+LANGUAGE plpgsql
+AS $$
+DECLARE table_name TEXT;
+BEGIN
+  FOR table_name IN (SELECT tablename FROM pg_tables
+                     WHERE schemaname = 'public')
+    LOOP
+      CALL proc_export(table_name, CONCAT(p_directory, table_name, '.', LOWER(p_format)), p_format, p_separator);
+    END LOOP;
+END
+$$;
+
+
+CREATE OR REPLACE PROCEDURE proc_import_all(p_directory text, p_format text, p_separator char(1) DEFAULT E'\t')
+LANGUAGE plpgsql
+AS $$
+DECLARE table_name TEXT;
+BEGIN
+  FOR table_name IN (SELECT tablename FROM pg_tables
+                     WHERE schemaname = 'public')
+    LOOP
+      CALL proc_import(table_name, CONCAT(p_directory, table_name, '.', LOWER(p_format)), p_format, p_separator);
+    END LOOP;
+END
+$$;
+
+CREATE OR REPLACE PROCEDURE proc_import (table_name text, file_name text, format text,
+                                         separator char(1) DEFAULT E'\t')
+LANGUAGE plpgsql
+AS $$
+
+DECLARE end_separator char(1) := (SELECT CASE WHEN (format = 'TSV')
+                                                THEN E'\t'
+                                              ELSE separator END);
+BEGIN
+  EXECUTE CONCAT('COPY ', table_name, ' FROM ', '''', file_name, '''',
+                 ' WITH (FORMAT CSV, HEADER, DELIMITER ',
+                 '''', end_separator, '''', ')');
+END
+$$;
 
 
